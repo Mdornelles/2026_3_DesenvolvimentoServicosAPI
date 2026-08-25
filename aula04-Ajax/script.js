663543030f537f8e8fb32e9965d99c78686c19b4
Miguel Martins Dorneles <migueldornelles2005@gmail.com>
2026-08-25 11:43:42 -0300

    Aula de AJAX, utilizando GET e POST

---
+++ aula04-Ajax/script.js
@@ -0,0 +1,34 @@
+function lerDados(){
+     document.getElementById("divDados").innerHTML = "Carregando..."
+    var req = new XMLHttpRequest()
+    req.onreadystatechange = function(){
+        // alert( "readyState: "+ this.readyState )
+        if(this.readyState == 4 && this.status == 200){
+            document.getElementById("divDados").innerHTML = this.responseText
+        }
+    }
+
+
+    req.open("GET" , "dados.txt" , true)
+    req.send()
+
+}
+function gerarNumeros(){
+     document.getElementById("divNumeros").innerHTML = "Carregando..."
+    var req = new XMLHttpRequest()
+    req.onreadystatechange = function(){
+        const divNumeros = document.getElementById("divNumeros")
+        if(this.readyState == 4 && this.status == 200){
+            divNumeros.innerHTML = this.responseText
+        }
+        if(this.readyState == 4 && this.status == 404){
+            divNumeros.innerHTML = "Código: " + this.status + "-" + this.statusText
+        }
+    }
+
+    const numero = document.getElementById("txtnumero").value
+    req.open("POST" , "servidor.php", true)
+    req.setRequestHeader("content-type" , "application/x-www-form-urlencoded")
+    req.send("valor" + numero)
+    
+}
